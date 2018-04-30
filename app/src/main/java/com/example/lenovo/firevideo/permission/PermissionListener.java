dfb92904b3f409d354e180d94955f4c53fddfc9e
“ <2106617419@qq.com>
2018-04-30 20:46:22 +0800

    纠正在用户注册时，判断是否有重名

---
+++ app/src/main/java/com/example/lenovo/firevideo/permission/PermissionListener.java
@@ -0,0 +1,22 @@
+package com.example.lenovo.firevideo.permission;
+
+import android.support.annotation.NonNull;
+
+/**
+ * Created by dfqin on 2017/1/20.
+ */
+
+public interface PermissionListener {
+
+    /**
+     * 通过授权
+     * @param permission
+     */
+    void permissionGranted(@NonNull String[] permission);
+
+    /**
+     * 拒绝授权
+     * @param permission
+     */
+    void permissionDenied(@NonNull String[] permission);
+}
