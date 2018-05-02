b87db6f3bea9bf55835aa514f6f8ae5c3e2e6e41
“ <2106617419@qq.com>
2018-05-02 11:10:59 +0800

    实现点击登陆按钮，显示加载动画。成功登陆则消失

--- app/src/main/java/com/example/lenovo/firevideo/activity/LoginActivity.java
+++ app/src/main/java/com/example/lenovo/firevideo/activity/LoginActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
@@ -17,8 +18,11 @@ import com.example.lenovo.firevideo.utils.PreferenceUtil;
 import com.example.lenovo.firevideo.R;
 import com.example.lenovo.firevideo.bean.UserInf;
 
+import java.util.List;
+
 import cn.bmob.v3.Bmob;
 import cn.bmob.v3.exception.BmobException;
+import cn.bmob.v3.listener.FindListener;
 import cn.bmob.v3.listener.SaveListener;
 //登陆界面
 public class LoginActivity extends AppCompatActivity {
@@ -131,6 +135,42 @@ public class LoginActivity extends AppCompatActivity {
             }
         });  */
         //2018.5.1.在登陆界面，登陆失败
+        cn.bmob.v3.BmobQuery<UserInf> query = new cn.bmob.v3.BmobQuery<UserInf>();
+        query.addWhereEqualTo("Username",userInfoBean.getUsername());
+        query.findObjects(new FindListener<UserInf>() {
+            @Override
+            public void done(List<UserInf> list, BmobException e) {
+                if(e==null){
+                    for (UserInf userInf : list) {
+                        //获得playerName的信息
+                        if (password.equals(userInf.getUserKey())){
+                            hideClick(avi);
+                            Toast.makeText(LoginActivity.this,"登录成功",Toast.LENGTH_SHORT).show();
+                            //跳转到主页   保存账号密码到SharedPreferences
+                            if(Login_Remember.isChecked()){
+                                PreferenceUtil.put(USER_NAME,username);
+                                PreferenceUtil.put(USER_PASSWORD,password);
+                            }
+                            Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+                            startActivity(intent);
+                            finish();
+                        }
+                        else {
+                            if(e.getErrorCode()==101){
+                                Toast.makeText(LoginActivity.this,"账号密码错误",Toast.LENGTH_SHORT).show();
+                            }else {
+                                Toast.makeText(LoginActivity.this,"登录失败",Toast.LENGTH_SHORT).show();
+                            }
+                        }
+
+                    }
+                }else{
+                    Log.i("bmob","失败："+e.getMessage()+","+e.getErrorCode());
+                }
+
+            }
+        });
+/*
         userInfoBean.save(new SaveListener<String>() {
             @Override
             public void done(String s, BmobException e) {
@@ -153,7 +193,7 @@ public class LoginActivity extends AppCompatActivity {
                     }
                 }
             }
-        });
+        });  */
     }
     public void hideClick(View view) {
         avi.hide();
